97534a47141dd1c7434dc474050641dbc0155a98
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-11-06 12:02:18 -0300

    funcional static page

--- insectrecognition/src/app.module.ts
+++ insectrecognition/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { SpeciesModule } from './species/species.module';
 
 @Module({
-  imports: [DatabaseModule, SpeciesModule],
+  imports: [
+    DatabaseModule,
+    SpeciesModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- insectrecognition/src/main.ts
+++ insectrecognition/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: '*', // Permite todas las peticiones, puedes restringir a un dominio específico si prefieres
+  });
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
