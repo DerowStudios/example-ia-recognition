b39ab77643da967ae4b1effecb5dee705a9475ff
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-11-11 20:26:36 -0300

    calibrador de imagen ok

--- insectrecognition/src/main.ts
+++ insectrecognition/src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', // Permite todas las peticiones, puedes restringir a un dominio específico si prefieres
-  });
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
